cd86f66650d7d3e76b8ce9dbe91ef22f74e18daf
Alexandre <Theve>
2013-12-05 17:45:58 +0100

    Mise en place de log4j

--- AndrEventsServer/src/main/java/andrevent/server/service/TestController.java
+++ AndrEventsServer/src/main/java/andrevent/server/service/TestController.java
@@ -1,5 +1,7 @@
 package andrevent.server.service;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class TestController {
-
+	private static final Logger logger = Logger.getLogger(TestController.class);
+	
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
 	public String getTest(@PathVariable String name, ModelMap model) {
+		System.out.println("test " + name);
+		logger.log(Level.INFO, "Message d'information");
 		model.addAttribute("name", name);
 		return "liste";
 	}
@@ -20,6 +25,7 @@ public class TestController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getDefaultTest(ModelMap model) {
 		System.out.println("test");
+		logger.log(Level.INFO, "Message d'information");
 		model.addAttribute("name", "this is default movie");
 		return "liste";
  
--- AndrEventsServer/src/main/java/andrevent/server/service/UserController.java
+++ AndrEventsServer/src/main/java/andrevent/server/service/UserController.java
@@ -2,6 +2,7 @@ package andrevent.server.service;
 
 import model.User;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+	private static final Logger logger = Logger.getLogger(UserController.class);
+	
 	/*@RequestMapping(value = "/{name}", method = RequestMethod.GET)
 	public String getUser(@PathVariable String name, ModelMap model) {
 		model.addAttribute("name", "User" + name);
