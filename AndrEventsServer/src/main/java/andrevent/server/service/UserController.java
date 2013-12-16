489feaed56790c180906072244e7cffd41892b5a
Alexandre <Theve>
2013-12-16 08:35:58 +0100

    ws d'ajout d'un utilisateur

--- AndrEventsServer/src/main/java/andrevent/server/service/UserController.java
+++ AndrEventsServer/src/main/java/andrevent/server/service/UserController.java
@@ -4,8 +4,10 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import andrevent.server.jpacontroller.UserJpaController;
@@ -40,5 +42,14 @@ public class UserController {
 			return userJpaController.findUserByLogin(login);
 
 		return new User();
+    }
+	
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+    public @ResponseBody User addUser(@RequestBody User user) {
+		logger.info("adding user : " + user);
+		if(user != null)
+			userJpaController.create(user);
+		
+		return user;
     }
 }
